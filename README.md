4bea39383458a4be0cb93294f3f3b28d9d1bbd16
Jason Mavandi <jmavandi@solutionreach.com>
2020-05-19 21:30:01 -0600

    working with dr site

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sweepstakes
 Automation for sweepstakes that I enter daily
+
+* Download [chromedriver](https://chromedriver.chromium.org/) and put it in the drivers folder. It must match your chrome version
---
+++ internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"context"
+
+	"github.com/mvndaai/ctxerr"
+	"github.com/spf13/viper"
+)
+
+// config value
+const (
+	// Personal Info
+	Email       = "email"
+	FirstName   = "first-name"
+	LastName    = "last-name"
+	PhoneNumber = "phone-number"
+	ZipCode     = "zip-code"
+)
+
+// InitConfig setups and reads in the config
+func InitConfig(ctx context.Context) error {
+	viper.SetDefault(Email, "")
+	viper.SetDefault(FirstName, "")
+	viper.SetDefault(LastName, "")
+	viper.SetDefault(PhoneNumber, "")
+	viper.SetDefault(ZipCode, "")
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	viper.AddConfigPath(".")
+	// viper.WriteConfigAs("example-config.json")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		ctxerr.QuickWrap(ctx, err)
+	}
+	return nil
+}
+
+// TODO use go generate to create the example config
